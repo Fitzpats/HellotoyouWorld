7085bdb3ffc0d41dd0c6e0e32dd98f2ba43521bd
Shayla Fitzpatrick <shaya.i.love.books@gmail.com>
2016-02-04 19:20:08 -0500

    Initial Commit

---
+++ HellotoyouWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HellotoyouWorld
+//
+//  Created by Shayla Fitzpatrick on 2/4/16.
+//  Copyright © 2016 Shayla Fitzpatrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
